9c262026f553061c33fca75af3e610430c1a526f
parameshbharadwaj <paramesh.javboy@outlook.jp>
2015-03-25 03:16:24 +0900

    Update ListRequest.java

--- src/test/java/com.p1/ListRequest.java
+++ src/test/java/com.p1/ListRequest.java
@@ -1,1 +1,24 @@
 
+package com.p1;
+
+import java.util.*;
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "demo")
+public class ListRequest {
+	
+	private List<Msg> listRequest = new ArrayList<Msg>();
+    
+	@XmlElement(name = "requesttable")
+	public List<Msg> getListRequest(){
+		return listRequest;	
+    }
+    
+	
+	public void setListRequest(List<Msg> listRequest)
+	{
+		this.listRequest = listRequest;	
+	}
+	
+}
+    
